e1735f5cbc0d9e95973c99be3bead43eee9ee9c6
daydreaam <dayingyeo@naver.com>
2024-11-18 18:10:27 +0900

    명언 8단계까지

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,17 +1,78 @@
 package com.ll;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.json.simple.JSONObject;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("== 명언 앱 ==");
+        Scanner scanner = new Scanner(System.in);
+        //TODO json, txt 파일 관련 읽어오는 과정 수행
+        int i = 1;
+        Moklok[] arr = new Moklok[300];
+        int num;
+        JSONObject json = new JSONObject();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        while(true){
+            System.out.print("명령) ");
+            String command = scanner.nextLine();
+
+            if (command.equals("등록")) {
+                System.out.print("명언 : ");
+                String saying = scanner.nextLine();
+                System.out.print("작가 : ");
+                String author = scanner.nextLine();
+                System.out.println(i + "번 명언이 등록되었습니다.");
+                arr[i] = new Moklok(i, author, saying);
+                i++;
+            }
+            else if(command.equals("종료")) {
+                for(int j = i; j > 0; j--){
+                    if(arr[j]!=null){
+                        // TODO json 파일, txt 파일 관련 수정 필요
+                        // json.put("id",arr[j].index);
+
+                    }
+                }
+                break;
+            }
+            else if(command.equals("목록")) {
+                System.out.println("번호/작가/명언");
+                System.out.println("----------------------");
+                for (int j = i; j > 0; j--) {
+                    if (arr[j] != null) {
+                        System.out.println(arr[j].index + " / " + arr[j].author + " / " + arr[j].saying);
+                    }
+                }
+            }
+            else if(command.substring(0,2).equals("삭제")){
+                num = Integer.parseInt(command.substring(6));
+                if(arr[num]!= null){
+                    arr[num] = null;
+                    System.out.println(num+"번 명언이 삭제되었습니다.");
+                }
+                else{
+                    System.out.println(num+"번 명언은 존재하지 않습니다.");
+                }
+            }
+            else if(command.substring(0,2).equals("수정")){
+                num = Integer.parseInt(command.substring(6));
+                if(arr[num]!= null){
+                    System.out.println("명언(기존) : "+arr[num].saying);
+                    System.out.print("명언 : ");
+                    arr[num].saying = scanner.nextLine();
+                    System.out.println("작가(기존) : "+arr[num].author);
+                    System.out.print("작가 : ");
+                    arr[num].author = scanner.nextLine();
+                }
+                else{
+                    System.out.println(num+"번 명언은 존재하지 않습니다.");
+                }
+            }
+
         }
+
+
     }
+
 }
---
+++ src/main/java/com/ll/Moklok.java
@@ -0,0 +1,12 @@
+package com.ll;
+
+public class Moklok {
+    int index;
+    String author;
+    String saying;
+    public Moklok(int index, String author, String saying){
+        this.index = index;
+        this.author = author;
+        this.saying = saying;
+    }
+}
